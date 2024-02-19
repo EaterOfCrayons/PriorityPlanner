46e94e847fed8d65a70921cc09e76acb4fcd182c
gamedoescool <hi_ishan@outlook.com>
2024-02-19 10:58:14 -0800

    skib dapdidadodop

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import json
 
 url = "http://localhost:5000/api/student/create-account"
 new_student = {
-    "name": "John",
+    "name": "John Cena",
     "account": {
-        "password": "1234",
+        "password": "1234",  #6969696969696696969696966969669696969696969669696969696969696969696969696969696696966969696969
         "classes": [10, 10],
         "grades": [90, 85],
         "assignments": [{"have sex": 1}],
@@ -17,10 +17,10 @@ response = requests.post(url, json=new_student)
 if response.status_code == 200:
     print("Account created successfully")
 else:
-    print("creation failed,", response.text)
+    print("creation x,", response.text)
 
 url = "http://localhost:5000/api/student/login"
-login_data = {"name": "John", "password": "1234"}
+login_data = {"name": "John Cena", "password": "1234"}
 response = requests.post(url, json=login_data)
 if response.status_code == 200:
     print("Login successful")
@@ -28,7 +28,7 @@ else:
     print("login failed:", response.text)
 
 url = "http://localhost:5000/api/student/classes"
-data = {"name": "John", "classes": ["id1", "id2"]}
+data = {"name": "John Cena", "classes": ["id1", "id2"]}
 response = requests.post(url, json=data)
 if response.status_code == 200:
     print("classes added successfully")
@@ -36,7 +36,7 @@ else:
     print("failed to add classes:", response.text)
 
 url = "http://localhost:5000/api/student/classes/grades"
-data = {"name": "John", "grades": [85, 90]}
+data = {"name": "John Cena", "grades": [85, 90]}
 response = requests.post(url, json=data)
 if response.status_code == 200:
     print("grades added successfully")
