471ef22031f2ab522742599b174e230f6bbeb7d8
gamedoescool <hi_ishan@outlook.com>
2024-02-19 14:59:12 -0800

    Fixed my code, added Clas functionality

--- classdata.js
+++ classdata.js
@@ -1,113 +1,113 @@
 class Clas{
     classGrades = {
-    }
+    };
     currentAssignments = {
 
-    }
+    };
     
-    accountData = { "David": { "password": "varmanismyfriend", "classes": [], "grades": [], "assignments": [], "assignment_weights": [] } }
 
     //the input gonna be like ["CSA","Pre Calc","Linglish 10 honors"]
     constructor(classes,grades) {
-        for (var i = 0; i < len(classes);i++) {
-            course = classes[i]
-            this.classGrades[course] = grades
-            this.currentAssignments[course] = []  //is this for the assignments for each class
+        for (var i = 0; i < classes.length;i++) {
+            course = classes[i];
+            this.classGrades[course] = grades;
+            this.currentAssignments[course] = [];  //is this for the assignments for each class
         }
             
     }
         
 
     //adds a previous assignment's grade to it (we dont care about the assignments name after its graded)
-    accessor addGradedProject(course, grade){
-        return this.classGrades[course] = (this.classGrades[course] + grade) / 2
+    addGradedProject(course, grade){
+        return this.classGrades[course] = (this.classGrades[course] + grade) / 2;
     }
 
 
-    accessor addAssignment(course, name, dueDate){
-        this.currentAssignments[course].append({"name": name, "did": dueDate})
+    addAssignment(course, name, dueDate){
+        this.currentAssignments[course].append({"name": name, "did": dueDate});
     } 
 
-    accessor getAllSortedScores() {
-        var scores = []
-        var names = []
+    getAllSortedScores() {
+        var scores = [];
+        var names = [];
         for (const key in this.currentAssignments){
-            values = this.currentAssignments[key]
+            values = this.currentAssignments[key];
             for (const assignment in values){
-                scores.append([key + assignment["name"], self.classGrades[key] - assignment["did"]])
-                names.append(assignment["did"])
+                scores.append([key + assignment["name"], this.classGrades[key] - assignment["did"]]);
+                names.append(assignment["did"]);
             }
         }
             
-        a = []
-        b = []
+        a = [];
+        b = [];
         for (const z in scores) {
-            a.append(z[1])
-            b.append(z[0])
+            a.append(z[1]);
+            b.append(z[0]);
         }
             
-        a = this.insertionSort(a,b,names)
+        a = this.insertionSort(a,b,names);
         return a
     }
        
-    accessor isAsending(l, index) {
+    isAsending(l, index) {
         if(index-1 > 0 && index+1<len(list)) {
-            return l[index-1] < l[index] < l[index+1]
+            return l[index-1] < l[index] < l[index+1];
         }
         else if(index == 0){
-            return l[0] < l[1]
+            return l[0] < l[1];
         }
         else{
-            return l[index-1] < l[index]
+            return l[index-1] < l[index];
         }
     }
        
-    accessor insertionSort(arr,anotherArr,thirdArr){
+    insertionSort(arr,anotherArr,thirdArr){
         for (var i = 1; i<len(arr);i++) {
-            key = arr[i]
-            key2 = anotherArr[i]
-            thirdKey = thirdArr[i]
-            j = i-1
+            key = arr[i];
+            key2 = anotherArr[i];
+            thirdKey = thirdArr[i];
+            j = i-1;
             while (j >= 0 && key < arr[j]) {
-                arr[j + 1] = arr[j]
-                anotherArr[j+1] = anotherArr[j]
-                thirdArr[j+1] = thirdArr[j]
-                j -= 1
+                arr[j + 1] = arr[j];
+                anotherArr[j+1] = anotherArr[j];
+                thirdArr[j+1] = thirdArr[j];
+                j -= 1;
             }
                 
-            arr[j + 1] = key
-            anotherArr[j + 1] = key2
-            thirdArr[j+1] = key3
-            return [thirdArr,anotherArr]
+            arr[j + 1] = key;
+            anotherArr[j + 1] = key2;
+            thirdArr[j+1] = key3;
+            return [thirdArr,anotherArr];
         }
     }
         
            
 
 }
-
+cles = 0;
 function thing() {
     console.log("monk");
+    cles = Clas(classes,grade)
 }
 
 let classes = [];
 let grade = [];
-    function changeText() {
-        var txt = document.getElementById('classInput').value;
-        var grd = document.getElementById('gradeInput').value;
+function changeText() {
+    console.log("here");
+    var txt = document.getElementById('classInput').value;
+    var grd = document.getElementById('gradeInput').value;
 
-        if (txt.trim() === '' || grd.trim() === '') {
-            alert("Please enter both class and grade.");
-            return false;
-        }
+    if (txt.trim() === '' || grd.trim() === '') {
+        alert("Please enter both class and grade.");
+        return false;
+    }
 
-        classes.push(txt);
-        grade.push(grd);
+    classes.push(txt);
+    grade.push(grd);
 
-        if (document.getElementById('classInput').value != null || document.getElementById('gradeInput').value != null) {
-            document.getElementById("classLabel").innerHTML += ("<br>" + txt + ", " + grd);
-
-}
+    if (document.getElementById('classInput').value != null || document.getElementById('gradeInput').value != null) {
+        document.getElementById("classLabel").innerHTML += ("<br>" + txt + ", " + grd);
+    }
 
     document.getElementById('classInput').value = "";
     document.getElementById('gradeInput').value = "";
