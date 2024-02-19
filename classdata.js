173f9764738eb68a03bfac2001b1e6295797584f
gamedoescool <hi_ishan@outlook.com>
2024-02-19 14:25:28 -0800

    Added clas object

--- classdata.js
+++ classdata.js
@@ -1,3 +1,91 @@
+class Clas{
+    classGrades = {
+    }
+    currentAssignments = {
+
+    }
+    
+    accountData = { "David": { "password": "varmanismyfriend", "classes": [], "grades": [], "assignments": [], "assignment_weights": [] } }
+
+    //the input gonna be like ["CSA","Pre Calc","Linglish 10 honors"]
+    constructor(classes,grades) {
+        for (var i = 0; i < len(classes);i++) {
+            course = classes[i]
+            this.classGrades[course] = grades
+            this.currentAssignments[course] = []  //is this for the assignments for each class
+        }
+            
+    }
+        
+
+    //adds a previous assignment's grade to it (we dont care about the assignments name after its graded)
+    accessor addGradedProject(course, grade){
+        return this.classGrades[course] = (this.classGrades[course] + grade) / 2
+    }
+
+
+    accessor addAssignment(course, name, dueDate){
+        this.currentAssignments[course].append({"name": name, "did": dueDate})
+    } 
+
+    accessor getAllSortedScores() {
+        var scores = []
+        var names = []
+        for (const key in this.currentAssignments){
+            values = this.currentAssignments[key]
+            for (const assignment in values){
+                scores.append([key + assignment["name"], self.classGrades[key] - assignment["did"]])
+                names.append(assignment["did"])
+            }
+        }
+            
+        a = []
+        b = []
+        for (const z in scores) {
+            a.append(z[1])
+            b.append(z[0])
+        }
+            
+        a = this.insertionSort(a,b,names)
+        return a
+    }
+       
+    accessor isAsending(l, index) {
+        if(index-1 > 0 && index+1<len(list)) {
+            return l[index-1] < l[index] < l[index+1]
+        }
+        else if(index == 0){
+            return l[0] < l[1]
+        }
+        else{
+            return l[index-1] < l[index]
+        }
+    }
+       
+    accessor insertionSort(arr,anotherArr,thirdArr){
+        for (var i = 1; i<len(arr);i++) {
+            key = arr[i]
+            key2 = anotherArr[i]
+            thirdKey = thirdArr[i]
+            j = i-1
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j]
+                anotherArr[j+1] = anotherArr[j]
+                thirdArr[j+1] = thirdArr[j]
+                j -= 1
+            }
+                
+            arr[j + 1] = key
+            anotherArr[j + 1] = key2
+            thirdArr[j+1] = key3
+            return [thirdArr,anotherArr]
+        }
+    }
+        
+           
+
+}
+
 let classes = [];
 let grade = [];
     function changeText() {
